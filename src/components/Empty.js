d93d9f193fc588773fef7bc7c9da5399a7d742c3
Satnam <satnam10virdi@gmail.com>
2020-09-22 21:51:59 +0530

    Add Image to Empty Component

--- src/components/Empty.js
+++ src/components/Empty.js
@@ -1,10 +1,23 @@
 import React from "react";
-import "../index.css"
+import "../index.css";
+import cat from "../assets/images/cat.svg";
 
 class Empty extends React.PureComponent {
 	render() {
-		return <p className="empty-list">¯\_(ツ)_/¯ Nothing To-Do yet!</p>
+		return (
+			<>
+				<p className="empty-list">
+					<img
+						className="empty-list-image"
+						src={cat}
+						alt=""
+						height="250"
+						width="250"
+					/>
+				</p>
+			</>
+		);
 	}
-};
+}
 
 export default Empty;
